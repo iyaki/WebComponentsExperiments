fdc7c97eb6555478fb22edc2b4d509c10d74441d
iyaki <ivanyaki96@gmail.com>
2021-03-08 00:37:49 -0300

    Refactor implementing alpine.js TODO: Find a way to call class methods from alpine.js directives

--- components/app-container.js
+++ components/app-container.js
@@ -2,31 +2,18 @@ import './inner-element.js'
 import './another-element.js'
 
 window.customElements.define('app-container', class extends HTMLElement {
-	showAnotherElement = false
-
 	connectedCallback() {
 		this.innerHTML = `
-		<button name="another-element">Add another element</button>
-		<inner-element></inner-element>
+		<div x-data="{ showAnotherElement: false }">
+
+			<button x-show="!showAnotherElement" @click="showAnotherElement = true">Add another element</button>
+			<button x-show="showAnotherElement" @click="showAnotherElement = false">Remove another element</button>
+			<inner-element></inner-element>
+			<template x-if="showAnotherElement">
+				<another-element x-show="showAnotherElement"></another-element>
+			</template>
+		</div>
 		`
 
-		this
-			.querySelector('button[name="another-element"]')
-			.addEventListener('click', this.toggleAnotherElement.bind(this))
-	}
-
-	toggleAnotherElement() {
-		this.showAnotherElement = !this.showAnotherElement
-
-		const button = this.querySelector('button[name="another-element"]')
-		if (this.showAnotherElement) {
-			button.innerText = 'Remove another element'
-
-			this.appendChild(document.createElement('another-element'))
-		} else {
-			button.innerText = 'Add another element'
-
-			this.querySelector('another-element').remove()
-		}
 	}
 })
--- components/inner-element.js
+++ components/inner-element.js
@@ -1,23 +1,10 @@
 window.customElements.define('inner-element', class extends HTMLElement {
-	constructor() {
-		super()
-		this.happines = ''
-	}
-
 	connectedCallback() {
 		this.innerHTML = `
-		<div>Yo soy otro web component${this.happines}</div>
-		<button name="happines">:D</button>
+		<div x-data="{ happinesLevel: 0 }">
+			<div x-text="'Yo soy otro web component' + ' :D'.repeat(happinesLevel)"></div>
+			<button @click="++happinesLevel">:D</button>
+		</div>
 		`
-
-		this
-			.querySelector('button[name="happines"]')
-			.addEventListener(
-				'click',
-				() => {
-					this.happines += ' :D'
-					this.connectedCallback()
-				}
-			)
 	}
 })
