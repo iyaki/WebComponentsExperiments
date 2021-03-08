0daaa4a89da786272bf612d11340ffe5de989322
iyaki <ivanyaki96@gmail.com>
2021-03-07 21:38:29 -0300

    Conditional rendering

--- components/app-container.js
+++ components/app-container.js
@@ -2,10 +2,31 @@ import './inner-element.js'
 import './another-element.js'
 
 window.customElements.define('app-container', class extends HTMLElement {
+	constructor() {
+		super()
+		this.showAnotherElement = false;
+	}
+
 	connectedCallback () {
+		let buttonText = 'Show another element'
+		if (this.showAnotherElement) {
+			buttonText = 'Hide another element'
+		}
+
 		this.innerHTML = `
+		<button name="another-element">${buttonText}</button>
 		<inner-element></inner-element>
-		<another-element></another-element>
 		`
+
+		if (this.showAnotherElement) {
+			this.innerHTML += `<another-element></another-element>`
+		}
+
+		this
+			.querySelector('button[name="another-element"]')
+			.addEventListener('click', () => {
+				this.showAnotherElement = !this.showAnotherElement
+				this.connectedCallback()
+			})
 	}
 })
